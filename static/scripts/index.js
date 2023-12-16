8ebffee361cc91519ed23f39ff915aa7bad4d4c1
sam-fsd <kabirusam037@gmail.com>
2023-12-16 14:11:33 +0300

    Authentification working

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,130 +1,3 @@
-// const tabs = document.querySelectorAll('.tab_btn');
-// const all_forms = document.querySelectorAll('.form');
-
-// tabs.forEach((tab, index) => {
-//   tab.addEventListener('click', (e) => {
-//     tabs.forEach((tab) => tab.classList.remove('active'));
-//     tab.classList.add('active');
-//     const line = document.querySelector('.line');
-//     line.style.width = e.target.offsetWidth + 'px';
-//     line.style.left = e.target.offsetLeft + 'px';
-
-//     all_forms.forEach((form) => form.classList.remove('active'));
-//     all_forms[index].classList.add('active');
-//   });
-// });
-
-// const uploadBtn = document.querySelector('.upload-btn');
-// const imagePreview = document.querySelector('.imagePreview');
-// const imageInput = document.querySelector('#imageInput');
-// const uploadImageIcon = document.querySelector('.upload-image-icon');
-// const uploadImageContainer = document.querySelector('.upload-image-container');
-// const removeImageBtn = document.querySelector('#remove-image-btn');
-// const cancelProductModalBtn = document.querySelector('.cancel-btn');
-// const postProductModal = document.querySelector('.post-product-modal');
-// const overylay = document.querySelector('#overlay');
-// const productMoreDetailsModal = document.querySelector('.product-more-details');
-// const moreDetailsBtn = document.querySelectorAll('.more-details');
-// const displayPostProductModal = document.querySelector(
-//   '.display-post-product-modal'
-// );
-// const cancelMoreDetailsModalBtn = document.querySelector(
-//   '.cancel-product-modal'
-// );
-
-// function previewImage(event) {
-//   const fileInput = event.target;
-
-//   const file = fileInput.files[0];
-
-//   if (file) {
-//     const reader = new FileReader();
-//     reader.onload = (e) => {
-//       imagePreview.setAttribute('src', e.target.result);
-//       imagePreview.style.display = 'block';
-//       uploadImageIcon.classList.add('hide');
-//       uploadBtn.classList.add('hide');
-//       uploadImageContainer.style.border = 'none';
-//       removeImageBtn.style.display = 'block';
-//     };
-//     reader.readAsDataURL(file);
-//   } else {
-//     imagePreview.setAttribute('src', '');
-//     imagePreview.style.display = 'none';
-//     uploadImageIcon.classList.remove('hide');
-//   }
-// }
-
-// //Trigger Post Product modal
-// displayPostProductModal.addEventListener('click', () => {
-//   postProductModal.style.display = 'block';
-//   overylay.style.display = 'block';
-// });
-
-// cancelProductModalBtn.addEventListener('click', () => {
-//   postProductModal.style.display = 'none';
-//   overylay.style.display = 'none';
-// });
-
-// //Trigger Product more details modal
-// moreDetailsBtn.forEach((btn) => {
-//   btn.addEventListener('click', () => {
-//     productMoreDetailsModal.style.display = 'block';
-//     overylay.style.display = 'block';
-//   });
-// });
-
-// //Close Product more details modal
-// cancelMoreDetailsModalBtn.addEventListener('click', () => {
-//   productMoreDetailsModal.style.display = 'none';
-//   overylay.style.display = 'none';
-// });
-
-// //Submitting Buyer farm details
-
-// const submitCustomerForm = () => {
-//   const form = document.getElementById('customer-form');
-//   const formData = new FormData(form);
-
-//   //Add the default query parameter
-//   formData.append('user_type', 'customer');
-
-//   fetch('/api/v1/auth/register', {
-//     method: 'POST',
-//     body: formData,
-//   })
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data);
-//       if (data.status === 'success') {
-//         window.location.href = 'pages/login.html';
-//       }
-//     })
-//     .catch((err) => console.log(err));
-// };
-
-// //Submitting Farmer farm details
-// const submitFarmerForm = () => {
-//   const form = document.getElementById('farmer-form');
-//   const formData = new FormData(form);
-
-//   //Add the default query parameter
-//   formData.append('user_type', 'farmer');
-
-//   fetch('/api/v1/auth/register', {
-//     method: 'POST',
-//     body: formData,
-//   })
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data);
-//       if (data.status === 'success') {
-//         window.location.href = 'pages/login.html';
-//       }
-//     })
-//     .catch((err) => console.log(err));
-// };
-
 $(document).ready(function () {
   const tabs = $('.tab_btn');
   const allForms = $('.form');
@@ -134,13 +7,8 @@ $(document).ready(function () {
   const uploadImageIcon = $('.upload-image-icon');
   const uploadImageContainer = $('.upload-image-container');
   const removeImageBtn = $('#remove-image-btn');
-  const cancelProductModalBtn = $('.cancel-btn');
-  const postProductModal = $('.post-product-modal');
-  const overlay = $('#overlay');
-  const productMoreDetailsModal = $('.product-more-details');
+  const loginBtn = $('#loginBtn');
   const moreDetailsBtn = $('.more-details');
-  const displayPostProductModal = $('.display-post-product-modal');
-  const cancelMoreDetailsModalBtn = $('.cancel-product-modal');
 
   $('.toggle').on('click', function () {
     if ($('.item').hasClass('active')) {
@@ -261,42 +129,74 @@ $(document).ready(function () {
 
     const jsonstring = JSON.stringify(jsonData);
 
-    //   fetch(`/api/v1/auth/register?user_type=${userType}`, {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: jsonstring,
-    //   })
-    //     .then((res) => res.json())
-    //     .then((data) => {
-    //       if (data.status === 'success') {
-    //         console.log(data.status);
-    //         console.log(data);
-    //         window.location.href = 'pages/login.html';
-    //       }
-    //     })
-    //     .catch((err) => console.log(err));
-    // };
-
-    // $('#submit-customer-form').on('click', function () {
-    //   submitForm('customer');
-    // });
-
-    // $('#submit-farmer-form').on('click', function () {
-    //   submitForm('farmer');
-
     $.ajax({
       url: `/api/v1/auth/register?user_type=${userType}`,
       type: 'POST',
       contentType: 'application/json',
       data: jsonstring,
       success: (data) => {
-        window.location.href = 'pages/login.html';
+        window.location.href = '/login';
       },
       error: (err) => {
         console.log(err);
       },
     });
   };
+
+  let isFormValid = true;
+
+  // Function to validate the form
+  function validateForm() {
+    const form = $('#loginForm')[0];
+    const inputs = form.querySelectorAll('input');
+    inputs.forEach((input) => {
+      input.addEventListener('input', () => {
+        if (input.value.trim() !== '') {
+          $(input).removeClass('error');
+        }
+      });
+
+      // Initial check for empty fields
+      if (!input.value.trim()) {
+        $(input).addClass('error');
+        isFormValid = false;
+      }
+    });
+    inputs.forEach((input) => {
+      if (!input.value.trim()) {
+        $(input).addClass('error');
+        isFormValid = false;
+      } else {
+        $(input).removeClass('error');
+      }
+    });
+  }
+
+  //Login user
+  const loginUser = () => {
+    //Validate form has not empty fields
+    validateForm();
+    if (!isFormValid) {
+      return;
+    }
+    const form = $('#loginForm')[0];
+
+    const formData = new FormData(form);
+
+    $.ajax({
+      url: '/api/v1/auth/token',
+      type: 'POST',
+      contentType: false,
+      data: formData,
+      processData: false,
+      success: (data) => {
+        window.location.href = '/home';
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  };
+
+  loginBtn.on('click', loginUser);
 });
